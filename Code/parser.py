1ebb8cac204690bf445dc2e77a373c81808c9f1b
jmuss07 <jmuss07@charlottesvilleschools.org>
2022-01-12 09:13:28 -0500

    fixed a thing

--- Code/parser.py
+++ Code/parser.py
@@ -12,9 +12,7 @@ while not cont:
             print()
             for i in phrase:
                 print(i)
-                if i == " ":
-                    print("Placeholder for a 'space'.\n")
-                elif i not in asl_dict:
+                if i not in asl_dict:
                     print(f"Sorry, I don't know how to sign {i}")
                 else:
                     asl_dict[i]()
